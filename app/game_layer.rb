4ea617b5034aadf668d0dd7d554ad95bbcf02f40
Jacob O'Donnell <jacobodonnell@gmail.com>
2012-07-06 09:02:41 -0400

    first commit

--- app/game_layer.rb
+++ app/game_layer.rb
@@ -1,12 +1,32 @@
 class GameLayer < CCLayer 
-  attr_accessor :player
+
+  def self.scene
+    scene = CCScene.node
+    layer = GameLayer.node
+
+    scene.addChild layer, z:1, tag: 1
+    scene
+  end
 
   def onEnter
     super
-    @player = Player.new
-    addChild @player.sprite
+    @frame_tick = 0
+
+    CCSpriteFrameCache.sharedSpriteFrameCache.addSpriteFramesWithFile("game_sprites.plist")
+
+    @bullets_batch = CCSpriteBatchNode.batchNodeWithFile("game_sprites.png")
+    addChild(@bullets_batch)
+
+    @bullets_batch.addChild CCSprite.spriteWithSpriteFrameName('circle.png')
 
     self.isTouchEnabled = true
+    schedule 'update'
+  end
+
+  def update
+
+    @frame_tick += 1
+    @frame_tick = 0 if @frame_tick == 1000000
   end
 
   def registerWithTouchDispatcher
@@ -20,17 +40,21 @@ class GameLayer < CCLayer
 
   def ccTouchMoved(touch, withEvent:event)
     location = convertTouchToNodeSpace(touch)
-    movePlayer location
+    touch_moved Point.new(location)
   end
 
-  def movePlayer position
-    @player.sprite.position = position
+  def ccTouchEnded(touch, withEvent:event)
+    touch_ended
   end
 
-  def self.scene
-    scene = CCScene.node
-    layer = GameLayer.node
-    scene.addChild layer, z:1, tag: 1
-    scene
+  def touch_began position
+    true
   end
+
+  def touch_moved position
+  end
+
+  def touch_ended
+  end
+
 end
